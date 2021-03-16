8997d0504763b10c4085d870fc6acb60ecfd8ef0
Youpele Michael <mjayjesus@gmail.com>
2021-03-16 15:25:55 +0100

    Added next and previous button

--- src/App.js
+++ src/App.js
@@ -10,6 +10,26 @@ function App() {
   const handlePage = (index) => {
     setPage(index)
   }
+
+  const nextPage = () => {
+    setPage((oldPage) => {
+      let nextPage = oldPage + 1
+      if (nextPage > data.length - 1) {
+        nextPage = 0
+      }
+      return nextPage
+    })
+  }
+  const prevPage = () => {
+    setPage((oldPage) => {
+      let prevPage = oldPage - 1
+      if (prevPage < 0) {
+        prevPage = data.length - 1
+      }
+      return prevPage
+    })
+  }
+
   // console.log(loading, data)
   useEffect(() => {
     if (loading) return //this line means if we are loading do nothing when we are done then do this
@@ -34,6 +54,9 @@ function App() {
         </div>
         {!loading && (
           <div className='btn-container'>
+            <button className='prev-btn' onClick={prevPage}>
+              prev
+            </button>
             {data.map((item, index) => {
               return (
                 <button
@@ -47,6 +70,9 @@ function App() {
                 </button>
               )
             })}
+            <button className='next-btn' onClick={nextPage}>
+              next
+            </button>
           </div>
         )}
       </section>
